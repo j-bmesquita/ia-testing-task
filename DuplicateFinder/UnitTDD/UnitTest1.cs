6cfb33c70bda88e680f7ce47b4881ee33409a418
j-bmesquita <j.bmesquita@outlook.com>
2022-04-01 10:22:38 +0200

    First Tests Defined

--- DuplicateFinder/UnitTDD/UnitTest1.cs
+++ DuplicateFinder/UnitTDD/UnitTest1.cs
@@ -3,34 +3,50 @@ using Xunit;
 
 namespace UnitTDD
 {
-    public class UnitTest1
+    public class NewFeatures
     {
         [Fact]
-        public void Test1()
+        public void WeCanReadBlackList() //we can read the Blacklist
         {
 
         }
 
         [Fact]
-        public void Test1()
+        public void InputEqualBlacklist() //we can compare user input with the Blacklist
         {
 
         }
 
         [Fact]
-        public void Test1()
+        public void SelectFromSize() //we can elect a file path from Size comparison
+        {
+
+        }
+
+        public void SelectFromNameandSize() //we can elect a file path from Size and Name comparison
+        {
+
+        }
+
+        public void SelectFromMD5() //we can elect a file path from MD5 comparison
         {
 
         }
 
         [Fact]
-        public void Test1()
+        public void WeCanRenameFolderWithNewName() //We can rename a folder with a new name, not in blacklist
         {
 
         }
 
+        public void StopRenamingInBlacklist() //We can rename a folder with a new name, not in blacklist
+        {
+
+        }
+
+
         [Fact]
-        public void Test1()
+        public void WeCannotRenameFolderWithExistingName() //We cannot rename a folder with the same name, else we would risk overwriting important files.
         {
 
         }
